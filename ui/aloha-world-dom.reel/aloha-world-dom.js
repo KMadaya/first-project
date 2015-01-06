6f7cdd37b4c5e1c43d62c07d964d12a77d2fd53b
Kevin Madaya <noreply>
2015-01-06 21:51:27 +0000

    Update component ui/aloha-world-dom.reel/aloha-world-dom.js

--- ui/aloha-world-dom.reel/aloha-world-dom.js
+++ ui/aloha-world-dom.reel/aloha-world-dom.js
@@ -18,8 +18,15 @@ exports.AlohaWorldDom = Component.specialize(/** @lends AlohaWorldDom# */ {
     draw:{
         value: function() {
             var div = document.createElement("div");
+            var fragment = document.createDocumentFragment();
             div.textContent = "Aloha World";
             this.element.appendChild(div);
+            fragment.appendChild(div);
+            fragment.appendChild(div.cloneNode(true));
+            fragment.appendChild(div.cloneNode(true));
+            fragment.firstChild.classList.add('AlohaWorldDom-left');
+            fragment.lastChild.classList.add('AlohaWorldDom-right');
+            this.element.appendChild(fragment);
         }
     }
 });
