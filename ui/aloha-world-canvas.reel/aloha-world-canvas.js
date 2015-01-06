68dd6827dd4c8d7edb76da2d042b32eccf4d0bd5
Kevin Madaya <noreply>
2015-01-06 21:55:16 +0000

    Add component aloha world canvas

---
+++ ui/aloha-world-canvas.reel/aloha-world-canvas.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/aloha-world-canvas.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class AlohaWorldCanvas
+ * @extends Component
+ */
+exports.AlohaWorldCanvas = Component.specialize(/** @lends AlohaWorldCanvas# */ {
+    constructor: {
+        value: function AlohaWorldCanvas() {
+            this.super();
+        }
+    }
+});
