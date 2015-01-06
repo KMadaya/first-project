366dee04d8b76abe78822825f42db3ceaa3970b3
Kevin Madaya <noreply>
2015-01-06 21:33:51 +0000

    Add component aloha world dom

---
+++ ui/aloha-world-dom.reel/aloha-world-dom.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/aloha-world-dom.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class AlohaWorldDom
+ * @extends Component
+ */
+exports.AlohaWorldDom = Component.specialize(/** @lends AlohaWorldDom# */ {
+    constructor: {
+        value: function AlohaWorldDom() {
+            this.super();
+        }
+    }
+});
