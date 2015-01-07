0c80756490a258476e0be6ebd8fac1fa76114ae1
Kevin Madaya <noreply>
2015-01-07 01:09:36 +0000

    Update component ui/main.reel/main.js

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -21,15 +21,15 @@ exports.Main = Component.specialize(/** @lends Main# */ {
 
     handleIncrementAction: {
         value: function (event) {
-            var value = Number(this.templateObjects.value.element.textContent);
-            this.templateObjects.value.element.textContent = value + 1;
+            var value = Number(this.templateObjects.Total.element.textContent);
+            this.templateObjects.Total.element.textContent = value + 1;
         }
     },
 
     handleDecrementAction: {
         value: function (event) {
-            var value = Number(this.templateObjects.value.element.textContent);
-            this.templateObjects.value.element.textContent = value - 1;
+            var value = Number(this.templateObjects.Total.element.textContent);
+            this.templateObjects.Total.element.textContent = value - 1;
         }
     }
 
